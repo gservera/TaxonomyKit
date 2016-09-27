2d774c4931890b0d141e7023b76940caf1951dee
Guillem Servera <guillem@gservera.com>
2016-09-27 15:51:58 +0200

    Initial Commit

---
+++ TaxonomyKit/TaxonomyKit.h
@@ -0,0 +1,19 @@
+//
+//  TaxonomyKit.h
+//  TaxonomyKit
+//
+//  Created by Guillem Servera Negre on 27/9/16.
+//  Copyright © 2016 Guillem Servera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaxonomyKit.
+FOUNDATION_EXPORT double TaxonomyKitVersionNumber;
+
+//! Project version string for TaxonomyKit.
+FOUNDATION_EXPORT const unsigned char TaxonomyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaxonomyKit/PublicHeader.h>
+
+
